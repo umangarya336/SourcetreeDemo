8449378c706b71d93a040178e8688ebfb84f37bc
Umang Arya <umangarya336@gmail.com>
2015-06-15 15:57:52 +0530

    FirstCommit

---
+++ STDemo1/STDemo1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  STDemo1
+//
+//  Created by Umang Arya on 15/06/15.
+//  Copyright (c) 2015 Umang Arya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
